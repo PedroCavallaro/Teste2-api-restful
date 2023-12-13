c0787538441e5f86c1920f589fc522f08edba491
PedroCavallaro <pedrolemecavallaro@gmail.com>
2023-12-13 03:55:55 -0300

    test: testing deployment

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,7 +3,12 @@ require('dotenv').config();
 
 const conn = async () => {
     try {
-        await mongoose.connect(process.env.CONNECTION_STRING);
+        //Estava utilizando o process.env, porem para vocês testarem o código será necessário a string de conexão
+        // await mongoose.connect(process.env.CONNECTION_STRING);
+        //Deixarei ela hard coded
+        await mongoose.connect(
+            'mongodb+srv://pedro:bWqDwtUcdwSggfe8@teste.oqk2cce.mongodb.net/?retryWrites=true&w=majority'
+        );
         console.log('conectado!');
     } catch (err) {
         console.log(err);
